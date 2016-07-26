90e8ee22b3db2ff4f64e3ef9b0d9b2fad45b9818
Colin <colin.diesh@gmail.com>
2016-07-26 12:44:01 -0500

    Modular css

--- src/JBrowse/Browser.js
+++ src/JBrowse/Browser.js
@@ -434,7 +434,7 @@ fatalError: function( error ) {
             var container = this.container || document.body;
             var thisB = this;
 
-            dojo.addClass( document.body, this.config.theme || "tundra"); //< tundra dijit theme
+            dojo.addClass( container, this.config.theme || "tundra"); //< tundra dijit theme
 
             if( !Util.isElectron() ) {
                 require([
@@ -651,7 +651,7 @@ initView: function() {
 
         //set up top nav/overview pane and main GenomeView pane
         dojo.addClass( this.container, "jbrowse"); // browser container has an overall .jbrowse class
-        dojo.addClass( document.body, this.config.theme || "tundra"); //< tundra dijit theme
+        dojo.addClass( this.container, this.config.theme || "tundra"); //< tundra dijit theme
 
         var topPane = dojo.create( 'div',{ style: {overflow: 'hidden'}}, this.container );
 
